8bbc78935dab91b169bc455f46359ea8893b045e
JhanakBhargava11 <jhanak.bhargava@xebia.com>
2020-06-30 16:37:54 +0530

    Loggin

---
+++ JwtSpringSecurity/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id,username,password,active,roles) VALUES(1,'jhanak.bhargava@xebia.com','password',True,'ROLE_USER');
+INSERT INTO users (id,username,password,active,roles) VALUES(2,'eshaan.mangal@xebia.com','password',True,'ROLE_USER');
+INSERT INTO users (id,username,password,active,roles) VALUES(3,'raghav.sindhwani@xebia.com','password',True,'ROLE_USER');
+INSERT INTO users (id,username,password,active,roles) VALUES(4,'anjali.akansha@xebia.com','password',True,'ROLE_USER');
+INSERT INTO users (id,username,password,active,roles) VALUES(4,'rituja.dixit@xebia.com','password',True,'ROLE_USER');
+
